c9393738a5200111cfc85aa613c39d46407a7522
Sebastian Krämer <sebkraemer@posteo.org>
2020-04-26 21:59:49 +0200

    use catch test for demo case

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,12 +1,39 @@
+#include "matrixblobfinder.hpp"
+
 #include <catch2/catch.hpp>
 
-unsigned int Factorial(unsigned int number) {
-    return number <= 1 ? number : Factorial(number - 1) * number;
-}
+TEST_CASE("MatrixBlobFinder", "[blobfinder]")
+{
+    SECTION("Demo")
+    {
+        // clang-format off
+        Matrix testMatrix{ 5, 7, {
+            0, 0, 0, 1, 0, 0, 0,
+            0, 1, 0, 1, 1, 1, 0,
+            1, 1, 0, 1, 1, 0, 0,
+            0, 0, 0, 0, 0, 0, 1,
+            0, 0, 0, 0, 1, 1, 1 }
+        };
+        // clang-format on
 
-TEST_CASE("Factorials are computed", "[factorial]") {
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
+        auto const blobs = findBlobs(testMatrix);
+        CHECK(blobs.size() == 3);
+        auto blobIndices = getLargestBlob(blobs);
+        CHECK(blobIndices.size() == 6);
+
+        // a custom Catch matcher would be nice here..
+        for (unsigned expectedBlobIdx : {3u, 10u, 11u, 12u, 17u, 18u})
+        {
+            CAPTURE(expectedBlobIdx);
+            CHECK(!blobIndices.extract(expectedBlobIdx).empty());
+        }
+    }
+
+    SECTION("1 0 0 0")
+    {
+        Matrix testMatrix{1, 4, {1, 0, 0, 0}};
+        auto const blobs = getLargestBlob(findBlobs(testMatrix));
+        CHECK(blobs.size() == 1);
+        CHECK(blobs.find(0) != blobs.end()); // first matrix entry is detected
+    }
 }
